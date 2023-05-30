91637e4435655c5e40ec22b64af91133ae4a2432
hereabran <here.abran@gmail.com>
2023-05-31 03:00:40 +0700

    update .gitignore

---
+++ app/tests/test_main.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+
+import os, sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_home():
+	response = client.get("/")
+	assert response.status_code == 200
+	assert response.json() == "Hello World"
